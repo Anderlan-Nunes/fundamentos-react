fc36affce579c293723ca4fa79ad72fceb9b8a9a
loja <loja5@gmail.com>
2022-12-23 20:48:48 -0200

    criando o primeiro componente

---
+++ src/components/basicos/Primeiro.js
@@ -0,0 +1,17 @@
+export default function Primeiro() {
+    const msg = 'Seja bem vindo'
+    return (
+        <div>
+            <h2>Primeiro Componente!</h2>
+            <p>{ msg }</p>
+        </div>
+    )
+}
+
+/**
+ * componentes- criar com a primeira letra maiuscula
+
+indentar na linha seguinto só com parentes 
+return (
+)
+ */
--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+import './index.css'
 import ReactDom from 'react-dom'; // a partir dele nós vamos usar para apartir dele mostrar alguma coisa na tela
+import React from 'react';
+
+import Primeiro from './components/basicos/Primeiro';
 
 const el = document.getElementById('root')
-ReactDom.render('Ola Mundo!!!',el);
+
+ReactDom.render(
+    <div>
+        <Primeiro></Primeiro>
+    </div>,
+    el);
