37df51a62dc62ae57cb0b420eda196172bf848ad
loja <loja5@gmail.com>
2022-12-23 22:18:17 -0200

    Criando Componente com propriedade

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDom from 'react-dom'; // a partir dele nós vamos usar para apartir 
 import React from 'react';
 
 import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
 
 const el = document.getElementById('root')
 
 ReactDom.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro
+           titulo="situação do Aluno," 
+           aluno= "Carlos Silva "
+           nota= {9.3}
+        />
+        <ComParametro
+           titulo="situação da Aluna," 
+           aluno= "Ana Silva "
+           nota= {6.9}
+        />
     </div>,
     el);
