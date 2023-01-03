40ae587aa8ecd1b36750b1a8eb601e1b98ab1fe7
loja <loja5@gmail.com>
2023-01-03 19:54:58 -0200

    colocando os componentes como filho dentro de cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Aleatorio from './components/basicos/Aleatorio';
+import Card from './components/layout/card';
+
 
 export default _ =>
+    
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Aleatorio
-            min={1}
-            max={12}
-        />
+        <Card titulo = "#01 - Número aleatório">
+            <Aleatorio min={1} max={12} />
+        </Card>
+        <Card titulo = "#02 - Com parâmetro 2">
+            <ComParametro
+                titulo="situação do Aluno,"
+                aluno="Carlos Silva "
+                nota={9.3}
+            />
+        </Card>
+        <Card titulo = "#03 - Com parâmetro 2">
+            <ComParametro
+                titulo="situação da Aluna,"
+                aluno="Ana Silva "
+                nota={6.9}
+            />
+        </Card>
+        <Card titulo = "#04 - Primeiro componente">
+            <Primeiro></Primeiro>
+        </Card>
         
-        <Primeiro></Primeiro>
-        <ComParametro
-            titulo="situação do Aluno,"
-            aluno="Carlos Silva "
-            nota={9.3}
-        />
-        <ComParametro
-            titulo="situação da Aluna,"
-            aluno="Ana Silva "
-            nota={6.9}
-        />
+
+
     </div>
