9b2b0cb388168124fbab782381067acecfc35d24
loja <loja5@gmail.com>
2023-01-03 21:29:00 -0200

    aplicando flexbox nos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Aleatorio from './components/basicos/Aleatorio';
@@ -5,30 +6,29 @@ import Card from './components/layout/card';
 
 
 export default _ =>
-    
-    <div id="app">
+
+    <div className='App'>
         <h1>Fundamentos React</h1>
-        <Card titulo = "#01 - Número aleatório">
-            <Aleatorio min={1} max={12} />
-        </Card>
-        <Card titulo = "#02 - Com parâmetro 2">
-            <ComParametro
-                titulo="situação do Aluno,"
-                aluno="Carlos Silva "
-                nota={9.3}
-            />
-        </Card>
-        <Card titulo = "#03 - Com parâmetro 2">
-            <ComParametro
-                titulo="situação da Aluna,"
-                aluno="Ana Silva "
-                nota={6.9}
-            />
-        </Card>
-        <Card titulo = "#04 - Primeiro componente">
-            <Primeiro></Primeiro>
-        </Card>
-        
-
-
+        <div className='Cards'>
+            <Card titulo="#01 - Número aleatório">
+                <Aleatorio min={1} max={12} />
+            </Card>
+            <Card titulo="#02 - Com parâmetro 2">
+                <ComParametro
+                    titulo="situação do Aluno,"
+                    aluno="Carlos Silva "
+                    nota={9.3}
+                />
+            </Card>
+            <Card titulo="#03 - Com parâmetro 2">
+                <ComParametro
+                    titulo="situação da Aluna,"
+                    aluno="Ana Silva "
+                    nota={6.9}
+                />
+            </Card>
+            <Card titulo="#04 - Primeiro componente">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
