c6a9fe2192312db29641d210b3680addd8a931bb
term02 <ter02@gmail.com>
2023-01-30 19:47:58 -0300

    dividindo o componente de classe em outros componentes

--- src/components/contadores/Classe/Contador2.jsx
+++ src/components/contadores/Classe/Contador2.jsx
@@ -1,6 +1,8 @@
 import '../Contador.css'
 import { Component } from "react";
 import Botoes from './Botoes';
+import Display from './Display';
+import PassoForm from './PassoForm';
 
 class Contador extends Component {
     state = {
@@ -20,9 +22,9 @@ class Contador extends Component {
         })
     }
 
-    setPasso = (evento) => {
+    setPasso = (novoPasso) => {
         this.setState({
-            passo: +evento.target.value,
+            passo: novoPasso,
         })
     }
 
@@ -30,15 +32,9 @@ class Contador extends Component {
         return (
             <div className='Contador'>
                 <h2>Contador</h2>
-                <h3>{this.state.numero}</h3>
+                <Display numero= {this.state.numero}/>
                 <div>
-                    <label htmlFor="passoInput">Passo: </label>
-                    <input 
-                        type="number" 
-                        id="passoInput"
-                        value={this.state.passo} 
-                        onChange={this.setPasso}
-                    />
+                    <PassoForm passo={this.state.passo} setarPasso={this.setPasso}/>
                 </div>
                 <Botoes incrementar= {this.inc} decrementar= {this.dec}/>
             </div>
