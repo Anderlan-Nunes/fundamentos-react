ccdabb4523cf4c5dd8023bb4016ba1285ad7c5c5
term02 <ter02@gmail.com>
2023-01-30 21:00:19 -0300

    dividindo o componente funcao

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Input from './components/formulario/Input';
 import Contador from './components/contadores/classe/Contador';
 import Contador2 from './components/contadores/classe/Contador2';
 import ContadorFuncao from './components/contadores/funcao/ContadorFuncao';
+import ContadorFuncao2 from './components/contadores/funcao/ContadorFuncao2';
 
 
 export default _ =>
@@ -21,20 +22,23 @@ export default _ =>
     <div className='App'>
         <h1>Fundamentos React</h1>
         <div className='Cards'>
+            <Card titulo="#14 - Contador Função2" color="#808080">
+                <ContadorFuncao2 />
+            </Card>
             <Card titulo="#13 - Contador2" color="#363636">
-                <Contador2/>
+                <Contador2 />
             </Card>
             <Card titulo="#12 - Contador Função" color="violet">
-                <ContadorFuncao/>
+                <ContadorFuncao />
             </Card>
             <Card titulo="#11 - Contador" color="#3A9E55">
-                <Contador/>
+                <Contador />
             </Card>
             <Card titulo="#10 - Componente controlado (Input)" >
-                <Input/>
+                <Input />
             </Card>
             <Card titulo="#9 - Comunicação Indireta" color="yellow">
-                <IndiretaPai/>
+                <IndiretaPai />
             </Card>
             <Card titulo="#8 - Comunicação Direta" color="#3A9AD9">
                 <DiretaPai></DiretaPai>
@@ -47,9 +51,9 @@ export default _ =>
             </Card>
             <Card titulo="#05 - Familia">
                 <Familia sobrenome="lopes">
-                <FamiliaMembro nome="Pedro" />
-                <FamiliaMembro nome="maria" />
-                <FamiliaMembro nome="mira" />
+                    <FamiliaMembro nome="Pedro" />
+                    <FamiliaMembro nome="maria" />
+                    <FamiliaMembro nome="mira" />
                 </Familia>
             </Card>
             <Card titulo="#04 - Número aleatório" color="#080">
